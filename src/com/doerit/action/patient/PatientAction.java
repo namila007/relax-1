68836daa8eb2285e74d0982c71dc4f5bc206e1f1
Prabhath <prabhath@doerit.com>
2017-07-13 06:06:26 +0530

    Patient Service module initiated

--- src/com/doerit/action/patient/PatientAction.java
+++ src/com/doerit/action/patient/PatientAction.java
@@ -1,11 +1,16 @@
 package com.doerit.action.patient;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.doerit.action.AbstractManagementAction;
 import com.doerit.model.Patient;
+import com.doerit.service.PatientService;
 
 public class PatientAction extends AbstractManagementAction{
 
 	private static final long serialVersionUID = 1L;
+	
+	@Autowired PatientService patientService;
 	
 	private Patient patient;
 
@@ -18,8 +23,22 @@ public class PatientAction extends AbstractManagementAction{
 	}
 	
 	public String save() {
-		//save code
-		return SUCCESS;
+		if(getId() != null && patient != null) {
+			patient.setId(generatePrimaryKey());
+			
+			int inserted = patientService.save(patient);
+			
+			if(inserted == 1) {
+				addActionMessage("Inserted");
+				return SUCCESS;
+			} else {
+				addActionError("Not inserted");
+				return ERROR;
+			}
+			
+		} else {
+			return INPUT;
+		}
 	}
 	
 	public String edit(){
---
+++ src/com/doerit/service/PatientService.java
@@ -0,0 +1,33 @@
+package com.doerit.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.doerit.dao.PatientMapper;
+import com.doerit.model.Patient;
+import com.doerit.util.State;
+
+@Service
+public class PatientService {
+
+	@Autowired
+	private PatientMapper patientMapper;
+	
+	public Patient viewById(String id) {
+		return patientMapper.selectByPrimaryKey(id);
+	}
+	
+	public int save(Patient patient) {
+		return patientMapper.insert(patient);
+	}
+	
+	public int update(Patient patient) {
+		return patientMapper.updateByPrimaryKeySelective(patient);
+	}
+	
+	public List<Patient> viewAll(State state) {
+		return patientMapper.viewAllByStatus(state.getDatabaseValue());
+	}
+}
